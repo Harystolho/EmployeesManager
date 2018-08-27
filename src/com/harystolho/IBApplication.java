f49fd15b2d4c4e3d190c5fb2753b3612830b633f
Harystolho <haristolhogamer@gmail.com>
2018-08-27 20:37:16 -0300

    Added initial parts

--- src/com/harystolho/IBApplication.java
+++ src/com/harystolho/IBApplication.java
@@ -3,13 +3,23 @@ package com.harystolho;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class IBApplication extends Application{
+public class IBApplication extends Application {
 
 	@Override
 	public void start(Stage window) throws Exception {
+		Main.setApp(this);
+
+		window.setTitle("Employees");
+		window.setWidth(1200);
+		window.setHeight(650);
+
 		
+		
+		window.show();
 	}
 
-	
-	
+	public void init(String[] args) {
+		launch(args);
+	}
+
 }
--- src/com/harystolho/Main.java
+++ src/com/harystolho/Main.java
@@ -2,10 +2,20 @@ package com.harystolho;
 
 public class Main {
 
+	private static IBApplication app;
+
 	public static void main(String[] args) {
-		
-		
-		
+
+		new IBApplication().init(args);
+
+	}
+
+	public static IBApplication getApp() {
+		return app;
+	}
+
+	public static void setApp(IBApplication app) {
+		Main.app = app;
 	}
 
 }
