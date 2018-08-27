a5b432183513d537faab1ceadb2c965518ac5fa9
Harystolho <haristolhogamer@gmail.com>
2018-08-27 20:31:34 -0300

    Init

---
+++ src/com/harystolho/IBApplication.java
@@ -0,0 +1,15 @@
+package com.harystolho;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class IBApplication extends Application{
+
+	@Override
+	public void start(Stage window) throws Exception {
+		
+	}
+
+	
+	
+}
---
+++ src/com/harystolho/Main.java
@@ -0,0 +1,11 @@
+package com.harystolho;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
